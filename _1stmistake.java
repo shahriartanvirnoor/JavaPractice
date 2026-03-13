2eb8dc609b768f7773863e5f6cca96a415349aad
shahriartanvirnoor <96464274+shahriartanvirnoor@users.noreply.github.com>
2026-03-13 14:58:17 +0600

    Fix class naming and string type in studentDesign
    Correct class name capitalization and fix string type.

---
+++ _1stmistake.java
@@ -0,0 +1,10 @@
+public class studentDesign{/*two public class can not exist in same file and class name must begin with Capital letters*/
+    string name;//string must be String
+    int id;
+    int year;
+}
+public class Student{
+    public static void main(String [] args){
+        System.out.println("Hello World");
+    }
+}
